98acdb0678639e7da84cae759a095eaae3cf463a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-11 15:59:54 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -253,10 +253,12 @@
 
           <ul class="nav flex-column mb-auto">
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#door-closed"/></svg>
-                Sign out
-              </a>
+                <div style="color:red;">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <svg class="bi"><use xlink:href="#door-closed"/></svg>
+                        Sign out
+                    </a>
+                </div>
             </li>
           </ul>
         </div>
