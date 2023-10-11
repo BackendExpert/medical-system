84de2db90382170e11215accea71f8fc8dad4747
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-11 16:27:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -216,7 +216,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="patients.php">
                 <svg class="bi"><use xlink:href="#people"/></svg>
                 Patients
               </a>
