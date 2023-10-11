73c24d09cfb67c69b2c889b2184d2f4b83d62c69
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-11 15:41:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -2,3 +2,15 @@
 <?php include("../function/function.php"); ?>
 
 <link rel="stylesheet" href="../../css/style.css">
+
+<div class="container">
+    <div class="main-card">
+        <div class="header-card">
+            Name : JehanKnady
+            Admin
+        </div>
+        <div class="user-pimg">
+            <img src="https://avatars.githubusercontent.com/u/138636749?v=4" alt="" class="user-img">
+        </div>
+    </div>
+</div>
