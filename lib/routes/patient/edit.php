45f05a01ff27945d3d0ec6d70f643ee922f84e7e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-16 22:21:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/patient/edit.php
+++ lib/routes/patient/edit.php
@@ -246,11 +246,11 @@
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2"><?php patient_id(); ?></h1>
-        <p><?php edit_patient_back(); ?></p>
+
       </div>
 
       <br>
-      <a href="show.php?id=<?php patient_id(); ?>"><button class="btn btn-primary">Back</button></a>
+      <a href="show.php?id=<?php edit_patient_back(); ?>"><button class="btn btn-primary">Back</button></a>
       
       <hr>
 
