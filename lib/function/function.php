425c6aaabb535a6964340da68a0169e88dcea4da
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-16 21:48:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -352,12 +352,12 @@
                 <div class='row'>
                     <div class='col-lg-6'>
                         Patient Mobile Number : 
-                        <input type='text' name='new_phone' value='".$patient_data['mobile_no']."'>
+                        <input type='text' name='new_phone' value='".$patient_data['mobile_no']."'  class='form-control'>
                     </div>
                     <div class-'col-lg-1'>&nbsp;&nbsp;&nbsp;</div>
                     <div class='col-lg-5'>
                         Patient First Date :
-                        <input type='text' value='".$patient_data['mobile_no']."' disabled>
+                        <input type='text' value='".$patient_data['mobile_no']."'  class='form-control' disabled>
                     </div>
                 </div>
             </form>
