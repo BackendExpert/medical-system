d4fc1023fca65f47905b2414dcc37061131e3b34
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-18 13:54:12 +0530

    Update

--- lib/routes/patients.php
+++ lib/routes/patients.php
@@ -203,7 +203,7 @@
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="admin.php">
                 <svg class="bi"><use xlink:href="#house-fill"/></svg>
                 Dashboard
               </a>
@@ -228,7 +228,7 @@
 
           <ul class="nav flex-column mb-auto">
             <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#" style="color: red;;">
+                    <a class="nav-link d-flex align-items-center gap-2" href="../views/logout.php" style="color: red;;">
                         <svg class="bi"><use xlink:href="#door-closed"/></svg>
                         Sign out
                     </a>
