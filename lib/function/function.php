4093f954795d21d8e317282161408dfad3a63d04
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-18 14:15:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -146,6 +146,9 @@
                             if($next_ch_date_r['ch_date'] == $check_date){
                                 $patient .="<td><a href=''><button class='btn btn-success'>Channel Patiemt</button></a></td>";
                             }
+                            elseif($next_ch_date_r['ch_date'] > $check_date){
+                                $patient .="<td><a href=''><button class='btn btn-danger'>Book another Date</button></a></td>";
+                            }
 
                             if($row['gender'] == "male"){
                                 $patient .="<td><i class='fas fa-male'></i> Male </td>";
