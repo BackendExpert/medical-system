2fe38a1abfff8767d92e2e4b02b14d831b91419a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-21 20:11:02 +0530

    Update

--- lib/routes/patient/show.php
+++ lib/routes/patient/show.php
@@ -259,6 +259,8 @@
       <br>
       <a href="../patients.php"><button class="btn btn-primary">Back</button></a>
       <a href="edit.php?id=<?php patient_nic(); ?>"><button class="btn btn-warning"><i class="fas fa-pen"></i> Edit Data</button></a>
+      <a href="edit.php?id=<?php patient_nic(); ?>"><button class="btn btn-warning"><i class="fas fa-pen"></i> Body Condition</button></a>
+      
       <hr>
 
       <?php show_patient(); ?>
